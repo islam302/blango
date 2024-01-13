93b5a6e94ebbd2053799f150e8ccdb72e55a9892
islam badran <islambadran39@gmail.com>
2024-01-13 20:26:39 +0000

    Finish django configurations

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,21 @@
 from django.shortcuts import render, redirect
-from blog.models import Post  # Import the Post model
+from blog.models import Post 
 from blog.forms import CommentForm
 
-def index(request):
-    if request.user.is_active:
-        if request.method == "POST":
-            comment_form = CommentForm(request.POST)
 
-            if comment_form.is_valid():
-                # Fetch a post from the database (adjust as needed based on your model)
-                post = Post.objects.first()
-
-                comment = comment_form.save(commit=False)
-                comment.content_object = post
-                comment.creator = request.user
-                comment.save()
-                return redirect(request.path_info)
-        else:
-            comment_form = CommentForm()
-
-        # Fetch a post from the database (adjust as needed based on your model)
-        post = Post.objects.first()
-    else:
-        comment_form = None
-        post = None
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
     
-    return render(
-        request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
-    )
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.content_object = post
+            comment.creator = request.user
+            comment.save()
+            return redirect(request.path_info)
+    else:
+        comment_form = CommentForm()
+
+    return render(request, "blog/post-detail.html", {"post": post, "comment_form": comment_form})
